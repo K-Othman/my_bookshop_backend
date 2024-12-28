cd057e61cdd153778cc05326d92d28a425b908ae
K-Othman <karim_othman88@hotmail.com>
2024-12-28 16:05:53 +0000

    created the books table and the categories table using sql and send the first 3 books

--- routes/books.js
+++ routes/books.js
@@ -4,8 +4,15 @@ const router = express.Router();
 const pool = require("../config/db.js");
 
 // Example route for testing
-router.get("/", (req, res) => {
-  res.send("Books API is working!");
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM books");
+    console.log(result.rows);
+
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: "Faild to fetch books" });
+  }
 });
 
 module.exports = router;
